7e65be32e5da3014daad44539b7ddd2fca5aa729
C4m1lle <camillemagne31@gmail.com>
2025-10-18 15:37:27 +0200

    main déplacé

--- distance.c
+++ distance.c
@@ -60,25 +60,3 @@ double tour_length(tTournee tour, DistanceFunc dist) {
     total += dist(tour->chemin[tour->current - 1], tour->chemin[0]);
     return total;
 }
-
-int main(void) {
-    tInstance a = create_instance(1, 10.0, 10.0);
-    tInstance b = create_instance(2, 20.0, 20.0);
-
-    printf("Distance EUCL_2D : %.3f\n", dist_eucl2d(a, b));
-    printf("Distance ATT     : %.3f\n", dist_att(a, b));
-
-    delete_instance(&a);
-    delete_instance(&b);
-
-    // Exemple GEO 
-    tInstance p = create_instance(1, 48.8566, 2.3522);
-    tInstance r = create_instance(2, 41.9028, 12.4964);
-    printf("Distance GEO     : %.3f km\n", dist_geo(p, r));
-
-    delete_instance(&p);
-    delete_instance(&r);
-
-    return 0;
-}
-
